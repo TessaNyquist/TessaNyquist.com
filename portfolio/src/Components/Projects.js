772406863ee73b45344ef8c37f22d4fb745b0643
TessaNyquist <tessanyquist@outlook.com>
2023-07-19 18:29:26 -0400

    Update skills

---
+++ portfolio/src/Components/Projects.js
@@ -0,0 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Calc_Img from './Images/Projects'
+
+/**
+ * This function creates a creates a Carosel for my Projects portion.
+ * 
+ * @returns returns a DropDown menu
+ */
+function Skills() {
+  return (
+  <Container>
+    <Card className="bg-dark text-white"> 
+      <Card.Header>PlaceHolder for Skills Portion</Card.Header>
+      <Card.Header>PlaceHolder for Skills Portion</Card.Header>
+    </Card> 
+  </Container>
+  );
+}
+
+export default Skills;
