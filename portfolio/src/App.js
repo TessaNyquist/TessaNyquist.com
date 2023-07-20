38e19c957f336715ec0443f7d043732cc559a705
TessaNyquist <tessanyquist@outlook.com>
2023-07-19 20:48:40 -0400

    fixed image sizing issue

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,12 +3,18 @@ import AboutMe from "./Components/AboutMe"
 import './App.css';
 import Skills from "./Components/Skills"
 import Container from "react-bootstrap/esm/Container";
+import Projects from "./Components/Projects"
+import ContactMeForm from "./Components/ContactMe";
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <Container>
+    <Container fluid = "xs">
+      <Header/>
       <AboutMe/>
       <Skills/>
+      <Projects/>
+      <ContactMeForm/>
     </Container>
   );
 }
--- portfolio/src/Components/Projects.js
+++ portfolio/src/Components/Projects.js
@@ -1,6 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
-import Calc_Img from './Images/Projects'
+import Calc_Img from './Images/modified-calculator.png'
 
 /**
  * This function creates a creates a Carosel for my Projects portion.
@@ -8,11 +8,17 @@ import Calc_Img from './Images/Projects'
  * @returns returns a DropDown menu
  */
 function Skills() {
+    let summary ="Background: To prepare for my Asteroids spinoff project, I decided to enhance my knowledge of React through a smaller project with guided instructions. I followed a tutorial on building a calculator using React,  which allowed me to gain a deeper understanding of setting up a React application and leveraging its functionalities such as states and props.  I deviated from the tutorial and further customized the calculator's colors and incorporated additional features like square root and exponant function. In addition,  I intend to take the knowledge gained from the tutorial and apply it to my Asteroids spinoff. For reference, you can access the tutorial I followed at the following link: https://www.sitepoint.com/react-tutorial-build-calculator-app/";
   return (
   <Container>
     <Card className="bg-dark text-white"> 
-      <Card.Header>PlaceHolder for Skills Portion</Card.Header>
-      <Card.Header>PlaceHolder for Skills Portion</Card.Header>
+        <Card.Header>About Me</Card.Header>
+        <Card.Img variant="top" src={Calc_Img} />
+        <Card.Body>
+            <Card.Text>{summary}</Card.Text>
+        </Card.Body>
+        <Card.Header>PlaceHolder for Skills Portion</Card.Header>
+        <Card.Header>PlaceHolder for Skills Portion</Card.Header>
     </Card> 
   </Container>
   );
