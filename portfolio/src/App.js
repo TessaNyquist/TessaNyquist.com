1cd4acbc8cc8cc4f8fb52f1caac5d2ebb00675db
TessaNyquist <tessanyquist@outlook.com>
2023-07-20 18:27:14 -0400

    Remove header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,7 +5,6 @@ import Skills from "./Components/Skills"
 import Container from "react-bootstrap/esm/Container";
 //import Projects from "./Pages/Projects"
 import ContactMeForm from "./Components/ContactMe";
-import Header from "./Components/Header";
 import NavBar from "./Components/NavBar";
 
 function App() {
